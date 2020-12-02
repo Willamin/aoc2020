674de1b353381248e3880cb03daf3e647990797e
Will Lewis <will@wflewis.com>
2020-12-02 18:16:12 -0500

    Add a regex approach

--- src/day02.cr
+++ src/day02.cr
@@ -37,12 +37,29 @@ def day02_part2(input : String) : String
   good_password_count.to_s
 end
 
+def day02_part2_faster(input : String) : String
+  good_password_count = 0
+  input.lines.each do |line|
+    parts = line.split(/[- :]/, remove_empty: true)
+    a = parts[0].to_i32
+    b = parts[1].to_i32
+    letter = parts[2]
+    password = parts[3]
+
+    if (password[a - 1]?.to_s == letter) ^ (password[b - 1]?.to_s == letter)
+      good_password_count += 1
+    end
+  end
+  good_password_count.to_s
+end
+
 # --- Boilerplate
 Card.puts("Day 02")
 input = STDIN.gets_to_end
 
 Bench(String).setup("") { |bm|
-  bm.run "part 1" { day02_part1(input) }
-  bm.run "part 2" { day02_part2(input) }
+  bm.run "part 1 scanner" { day02_part1(input) }
+  bm.run "part 2 scanner" { day02_part2(input) }
+  bm.run "part 2 regex" { day02_part2_faster(input) }
 }
   .tap { |b| puts(b) }
